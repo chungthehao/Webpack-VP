9cc2bfdf60e9a7c0bea679cda688e8e907da59e0
Henry Chung <chungthehao@gmail.com>
2020-04-24 23:17:05 +0700

    Serving HTML Pages via Express

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
+const path = require("path");
+const fs = require("fs");
 const express = require("express");
 
 const app = express();
 
-app.get("/", function (res, req) {
-  return req.send("Some dummy content");
+app.get("/", function (req, res) {
+  const pathToHtmlFile = path.resolve(__dirname, "../dist/hello-world.html");
+  const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, "utf-8");
+
+  return res.send(contentFromHtmlFile);
 });
 
 app.listen(3000, function () {
