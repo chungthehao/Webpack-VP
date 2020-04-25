38a1c374d8dd277f301f667fbf5294292c9f9d71
Henry Chung <chungthehao@gmail.com>
2020-04-25 16:52:28 +0700

    Using FontAwesome with Webpack

--- src/hello-world.js
+++ src/hello-world.js
@@ -1,7 +1,15 @@
 import "./index.scss";
 
+// library: specify which icons we want to use
+// dom: replace any existing <i></i> with svg
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+
 import HelloWorldButton from "./components/hello-world-button/hello-world-button";
 import Heading from "./components/heading/heading";
+
+library.add(faSpinner);
+dom.watch(); // watch các thẻ i để thay bằng svg (observer)
 
 const heading = new Heading();
 heading.render("hello world");
